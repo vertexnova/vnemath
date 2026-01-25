3d53a310545a913b4f727e4571928a289f94f6d8
ayadav-macOs <yadav.ajeetsingh2020@gmail.com>
2026-01-25 05:05:35 -0500

    Enhance Vec2f and Vec3f classes with noexcept specifiers for improved performance
    Changes:
    - Added noexcept specifiers to constructors, member functions, and operator overloads in `Vec2f` and `Vec3f` classes to indicate that these functions do not throw exceptions.
    - Updated return types of various methods to include [[nodiscard]] for better usage tracking and to prevent unintentional discarding of return values.
    - Improved code clarity and maintainability by ensuring consistent use of noexcept across the math classes.

--- include/vertexnova/math/vec2f.h
+++ include/vertexnova/math/vec2f.h
@@ -35,101 +35,101 @@ namespace vne::math {
  */
 class Vec2f : public glm::vec2 {
    public:
-    Vec2f()
+    Vec2f() noexcept
         : glm::vec2(0.0f, 0.0f) {}
-    Vec2f(float x_value, float y_value)
+    Vec2f(float x_value, float y_value) noexcept
         : glm::vec2(x_value, y_value) {}
-    Vec2f(const float xy)
+    Vec2f(const float xy) noexcept
         : glm::vec2(xy, xy) {}
-    Vec2f(const Vec2f& v)
+    Vec2f(const Vec2f& v) noexcept
         : glm::vec2(v) {}
-    Vec2f(const glm::vec2& v)
+    Vec2f(const glm::vec2& v) noexcept
         : glm::vec2(v.x, v.y) {}
 
    public:
-    float* getPtr();
-    const float* getPtr() const;
+    [[nodiscard]] float* getPtr() noexcept;
+    [[nodiscard]] const float* getPtr() const noexcept;
 
    public:
-    Vec2f abs() const;
-    float length() const;
-    float lengthSquare() const;
-    Vec2f normalize() const;
-    Vec2f min(const Vec2f& v) const;
-    Vec2f max(const Vec2f& v) const;
-    float minComponent() const;
-    float maxComponent() const;
-    float dot(const Vec2f& v) const;
-    float cross(const Vec2f& v) const;
-    float distance(const Vec2f& v) const;
-    Vec2f rotate(const Vec2f& axis, float angle) const;
-    Vec2f reflect(const Vec2f& normal) const;
-    Vec2f refract(const Vec2f& normal, float mu) const;
-    Vec2f project(const Vec2f& v) const;
-    Vec2f reject(const Vec2f& v) const;
-    void decomposeVec(const Vec2f& v, Vec2f& proj, Vec2f& perp) const;
-    Vec2f perpendicular() const;
+    [[nodiscard]] Vec2f abs() const noexcept;
+    [[nodiscard]] float length() const noexcept;
+    [[nodiscard]] float lengthSquare() const noexcept;
+    [[nodiscard]] Vec2f normalize() const;
+    [[nodiscard]] Vec2f min(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f max(const Vec2f& v) const noexcept;
+    [[nodiscard]] float minComponent() const noexcept;
+    [[nodiscard]] float maxComponent() const noexcept;
+    [[nodiscard]] float dot(const Vec2f& v) const noexcept;
+    [[nodiscard]] float cross(const Vec2f& v) const noexcept;
+    [[nodiscard]] float distance(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f rotate(const Vec2f& axis, float angle) const noexcept;
+    [[nodiscard]] Vec2f reflect(const Vec2f& normal) const noexcept;
+    [[nodiscard]] Vec2f refract(const Vec2f& normal, float mu) const noexcept;
+    [[nodiscard]] Vec2f project(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f reject(const Vec2f& v) const noexcept;
+    void decomposeVec(const Vec2f& v, Vec2f& proj, Vec2f& perp) const noexcept;
+    [[nodiscard]] Vec2f perpendicular() const noexcept;
 
    public:
-    bool areSame(const Vec2f& v, float eps = kFloatEpsilon) const;
-    bool areAligned(const Vec2f& v, float eps = kFloatEpsilon) const;
-    bool isZero(float eps = kFloatEpsilon) const;
-    bool isNormalized(float eps = kFloatEpsilon) const;
+    [[nodiscard]] bool areSame(const Vec2f& v, float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool areAligned(const Vec2f& v, float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool isZero(float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool isNormalized(float eps = kFloatEpsilon) const noexcept;
 
    public:
-    Vec2f& composePolar(float radius, float angle);
-    void decomposePolar(float& radius, float& angle) const;
-    float angle() const;
-    float angle(const Vec2f& v) const;
-    float angle(const Vec2f& p1, const Vec2f& p2) const;
+    Vec2f& composePolar(float radius, float angle) noexcept;
+    void decomposePolar(float& radius, float& angle) const noexcept;
+    [[nodiscard]] float angle() const noexcept;
+    [[nodiscard]] float angle(const Vec2f& v) const noexcept;
+    [[nodiscard]] float angle(const Vec2f& p1, const Vec2f& p2) const noexcept;
 
    public:
-    Vec2f midPoint(const Vec2f& point) const;
-    Vec2f lerp(Vec2f& point, float factor) const;
+    [[nodiscard]] Vec2f midPoint(const Vec2f& point) const noexcept;
+    [[nodiscard]] Vec2f lerp(Vec2f& point, float factor) const noexcept;
 
    public:
-    Vec2f& operator=(const Vec2f& v);
-    Vec2f& operator+=(const float& scalar);
-    Vec2f& operator-=(const float& scalar);
-    Vec2f& operator*=(const float& scalar);
-    Vec2f& operator/=(const float& scalar);
-    Vec2f& operator+=(const Vec2f& v);
-    Vec2f& operator-=(const Vec2f& v);
-    Vec2f& operator*=(const Vec2f& v);
-    Vec2f& operator/=(const Vec2f& v);
-    Vec2f operator+(const float& scalar) const;
-    Vec2f operator-(const float& scalar) const;
-    Vec2f operator*(const float& scalar) const;
-    Vec2f operator/(const float& scalar) const;
-    Vec2f operator+(const Vec2f& v) const;
-    Vec2f operator-(const Vec2f& v) const;
-    Vec2f operator*(const Vec2f& v) const;
-    Vec2f operator/(const Vec2f& v) const;
-    bool operator==(const Vec2f& v) const;
-    bool operator!=(const Vec2f& v) const;
-    bool operator>(const Vec2f& v) const;
-    bool operator<(const Vec2f& v) const;
-    Vec2f operator-() const;
-    float& operator[](uint32_t index);
-    float operator[](uint32_t index) const;
+    Vec2f& operator=(const Vec2f& v) noexcept;
+    Vec2f& operator+=(const float& scalar) noexcept;
+    Vec2f& operator-=(const float& scalar) noexcept;
+    Vec2f& operator*=(const float& scalar) noexcept;
+    Vec2f& operator/=(const float& scalar) noexcept;
+    Vec2f& operator+=(const Vec2f& v) noexcept;
+    Vec2f& operator-=(const Vec2f& v) noexcept;
+    Vec2f& operator*=(const Vec2f& v) noexcept;
+    Vec2f& operator/=(const Vec2f& v) noexcept;
+    [[nodiscard]] Vec2f operator+(const float& scalar) const noexcept;
+    [[nodiscard]] Vec2f operator-(const float& scalar) const noexcept;
+    [[nodiscard]] Vec2f operator*(const float& scalar) const noexcept;
+    [[nodiscard]] Vec2f operator/(const float& scalar) const noexcept;
+    [[nodiscard]] Vec2f operator+(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f operator-(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f operator*(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f operator/(const Vec2f& v) const noexcept;
+    [[nodiscard]] bool operator==(const Vec2f& v) const noexcept;
+    [[nodiscard]] bool operator!=(const Vec2f& v) const noexcept;
+    [[nodiscard]] bool operator>(const Vec2f& v) const noexcept;
+    [[nodiscard]] bool operator<(const Vec2f& v) const noexcept;
+    [[nodiscard]] Vec2f operator-() const noexcept;
+    [[nodiscard]] float& operator[](uint32_t index) noexcept;
+    [[nodiscard]] float operator[](uint32_t index) const noexcept;
 
    public:
     friend std::ostream& operator<<(std::ostream& os, const Vec2f& vec);
     friend std::istream& operator>>(std::istream& is, Vec2f& vec);
 
    public:
-    static uint32_t dim();
-    static uint32_t size();
-    static Vec2f zero();
-    static Vec2f one();
-    static Vec2f xAxis();
-    static Vec2f yAxis();
-    static Vec2f normalized(const Vec2f& v);
-    static float distance(const Vec2f& v1, const Vec2f& v2);
-    static float dot(const Vec2f& v1, const Vec2f& v2);
-    static float cross(const Vec2f& v1, const Vec2f& v2);
-    static Vec2f midPoint(const Vec2f& p1, const Vec2f& p2);
-    static Vec2f lerp(const Vec2f& p1, const Vec2f& p2, float factor);
+    [[nodiscard]] static uint32_t dim() noexcept;
+    [[nodiscard]] static uint32_t size() noexcept;
+    [[nodiscard]] static Vec2f zero() noexcept;
+    [[nodiscard]] static Vec2f one() noexcept;
+    [[nodiscard]] static Vec2f xAxis() noexcept;
+    [[nodiscard]] static Vec2f yAxis() noexcept;
+    [[nodiscard]] static Vec2f normalized(const Vec2f& v);
+    [[nodiscard]] static float distance(const Vec2f& v1, const Vec2f& v2) noexcept;
+    [[nodiscard]] static float dot(const Vec2f& v1, const Vec2f& v2) noexcept;
+    [[nodiscard]] static float cross(const Vec2f& v1, const Vec2f& v2) noexcept;
+    [[nodiscard]] static Vec2f midPoint(const Vec2f& p1, const Vec2f& p2) noexcept;
+    [[nodiscard]] static Vec2f lerp(const Vec2f& p1, const Vec2f& p2, float factor) noexcept;
 };
 
 // Legacy type alias for backward compatibility
--- include/vertexnova/math/vec3f.h
+++ include/vertexnova/math/vec3f.h
@@ -33,121 +33,121 @@ namespace vne::math {
 
 class Vec3f : public glm::vec3 {
    public:
-    Vec3f()
-        : glm::vec3(0.0f) {};
-    Vec3f(float x_value, float y_value, float z_value)
-        : glm::vec3(x_value, y_value, z_value) {};
-    Vec3f(const float xyz)
-        : glm::vec3(xyz) {};
-    Vec3f(const Vec3f& v)
-        : glm::vec3(v) {};
-    Vec3f(const glm::vec3& v)
-        : glm::vec3(v.x, v.y, v.z) {};
-    Vec3f(const Vec2f& v)
-        : glm::vec3(v.x, v.y, 0.0f) {};
-    Vec3f(const glm::vec2& v)
-        : glm::vec3(v.x, v.y, 0.0f) {};
-    Vec3f(const Vec2f& v, float z_value)
-        : glm::vec3(v, z_value) {};
-    Vec3f(const glm::vec2& v, float z_value)
-        : glm::vec3(v, z_value) {};
+    Vec3f() noexcept
+        : glm::vec3(0.0f) {}
+    Vec3f(float x_value, float y_value, float z_value) noexcept
+        : glm::vec3(x_value, y_value, z_value) {}
+    Vec3f(const float xyz) noexcept
+        : glm::vec3(xyz) {}
+    Vec3f(const Vec3f& v) noexcept
+        : glm::vec3(v) {}
+    Vec3f(const glm::vec3& v) noexcept
+        : glm::vec3(v.x, v.y, v.z) {}
+    Vec3f(const Vec2f& v) noexcept
+        : glm::vec3(v.x, v.y, 0.0f) {}
+    Vec3f(const glm::vec2& v) noexcept
+        : glm::vec3(v.x, v.y, 0.0f) {}
+    Vec3f(const Vec2f& v, float z_value) noexcept
+        : glm::vec3(v, z_value) {}
+    Vec3f(const glm::vec2& v, float z_value) noexcept
+        : glm::vec3(v, z_value) {}
 
-    Vec2f xy();
-
-   public:
-    float* getPtr();
-    const float* getPtr() const;
+    [[nodiscard]] Vec2f xy() noexcept;
 
    public:
-    Vec3f abs() const;
-    float length() const;
-    float lengthSquare() const;
-    Vec3f normalize() const;
-    Vec3f min(const Vec3f& v) const;
-    Vec3f max(const Vec3f& v) const;
-    float minComponent() const;
-    float maxComponent() const;
-    float dot(const Vec3f& v) const;
-    Vec3f cross(const Vec3f& v) const;
-    float distance(const Vec3f& v) const;
-    Vec3f rotate(const Vec3f& axis, float angle) const;
-    Vec3f reflect(const Vec3f& normal) const;
-    Vec3f refract(const Vec3f& normal, float mu) const;
-    Vec3f project(const Vec3f& v) const;
-    Vec3f reject(const Vec3f& v) const;
-    void decomposeVec(const Vec3f& v, Vec3f& proj, Vec3f& perp) const;
-    Vec3f perpendicular(const Vec3f& v) const;
+    [[nodiscard]] float* getPtr() noexcept;
+    [[nodiscard]] const float* getPtr() const noexcept;
 
    public:
-    bool areSame(const Vec3f& v, float eps = kFloatEpsilon) const;
-    bool areAligned(const Vec3f& v, float eps = kFloatEpsilon) const;
-    bool isZero(float eps = kFloatEpsilon) const;
-    bool isNormalized(float eps = kFloatEpsilon) const;
-    bool isLinearDependent(const Vec3f& p1, const Vec3f& p2, float eps = kFloatEpsilon) const;
-    bool isLinearDependent(const Vec3f& v, float eps = kFloatEpsilon) const;
+    [[nodiscard]] Vec3f abs() const noexcept;
+    [[nodiscard]] float length() const noexcept;
+    [[nodiscard]] float lengthSquare() const noexcept;
+    [[nodiscard]] Vec3f normalize() const;
+    [[nodiscard]] Vec3f min(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f max(const Vec3f& v) const noexcept;
+    [[nodiscard]] float minComponent() const noexcept;
+    [[nodiscard]] float maxComponent() const noexcept;
+    [[nodiscard]] float dot(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f cross(const Vec3f& v) const noexcept;
+    [[nodiscard]] float distance(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f rotate(const Vec3f& axis, float angle) const noexcept;
+    [[nodiscard]] Vec3f reflect(const Vec3f& normal) const noexcept;
+    [[nodiscard]] Vec3f refract(const Vec3f& normal, float mu) const noexcept;
+    [[nodiscard]] Vec3f project(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f reject(const Vec3f& v) const noexcept;
+    void decomposeVec(const Vec3f& v, Vec3f& proj, Vec3f& perp) const noexcept;
+    [[nodiscard]] Vec3f perpendicular(const Vec3f& v) const noexcept;
 
    public:
-    Vec3f& composeSpherical(float rho, float theta, float phi);
-    void decomposeSpherical(float& rho, float& theta, float& phi) const;
-    Vec3f& composeCylindrical(float radius, float angle, float height);
-    void decomposeCylindrical(float& radius, float& angle, float& height) const;
-    float angle(const Vec3f& v) const;
-    float angle(const Vec3f& p1, const Vec3f& p2) const;
+    [[nodiscard]] bool areSame(const Vec3f& v, float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool areAligned(const Vec3f& v, float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool isZero(float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool isNormalized(float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool isLinearDependent(const Vec3f& p1, const Vec3f& p2, float eps = kFloatEpsilon) const noexcept;
+    [[nodiscard]] bool isLinearDependent(const Vec3f& v, float eps = kFloatEpsilon) const noexcept;
 
    public:
-    Vec3f midPoint(const Vec3f& point) const;
-    Vec3f lerp(Vec3f& point, float factor) const;
+    Vec3f& composeSpherical(float rho, float theta, float phi) noexcept;
+    void decomposeSpherical(float& rho, float& theta, float& phi) const noexcept;
+    Vec3f& composeCylindrical(float radius, float angle, float height) noexcept;
+    void decomposeCylindrical(float& radius, float& angle, float& height) const noexcept;
+    [[nodiscard]] float angle(const Vec3f& v) const noexcept;
+    [[nodiscard]] float angle(const Vec3f& p1, const Vec3f& p2) const noexcept;
 
    public:
-    Vec3f& operator=(const Vec3f& v);
-    Vec3f& operator+=(float scalar);
-    Vec3f& operator-=(float scalar);
-    Vec3f& operator*=(float scalar);
-    Vec3f& operator/=(float scalar);
-    Vec3f& operator+=(const Vec3f& v);
-    Vec3f& operator-=(const Vec3f& v);
-    Vec3f& operator*=(const Vec3f& v);
-    Vec3f& operator/=(const Vec3f& v);
-    Vec3f operator+(float scalar) const;
-    Vec3f operator-(float scalar) const;
-    Vec3f operator*(float scalar) const;
-    Vec3f operator/(float scalar) const;
-    Vec3f operator+(const Vec3f& v) const;
-    Vec3f operator-(const Vec3f& v) const;
-    Vec3f operator*(const Vec3f& v) const;
-    Vec3f operator/(const Vec3f& v) const;
-    bool operator==(const Vec3f& v) const;
-    bool operator!=(const Vec3f& v) const;
-    bool operator>(const Vec3f& v) const;
-    bool operator<(const Vec3f& v) const;
-    Vec3f operator-() const;
-    float& operator[](uint32_t index);
-    float operator[](uint32_t index) const;
+    [[nodiscard]] Vec3f midPoint(const Vec3f& point) const noexcept;
+    [[nodiscard]] Vec3f lerp(Vec3f& point, float factor) const noexcept;
+
+   public:
+    Vec3f& operator=(const Vec3f& v) noexcept;
+    Vec3f& operator+=(float scalar) noexcept;
+    Vec3f& operator-=(float scalar) noexcept;
+    Vec3f& operator*=(float scalar) noexcept;
+    Vec3f& operator/=(float scalar) noexcept;
+    Vec3f& operator+=(const Vec3f& v) noexcept;
+    Vec3f& operator-=(const Vec3f& v) noexcept;
+    Vec3f& operator*=(const Vec3f& v) noexcept;
+    Vec3f& operator/=(const Vec3f& v) noexcept;
+    [[nodiscard]] Vec3f operator+(float scalar) const noexcept;
+    [[nodiscard]] Vec3f operator-(float scalar) const noexcept;
+    [[nodiscard]] Vec3f operator*(float scalar) const noexcept;
+    [[nodiscard]] Vec3f operator/(float scalar) const noexcept;
+    [[nodiscard]] Vec3f operator+(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f operator-(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f operator*(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f operator/(const Vec3f& v) const noexcept;
+    [[nodiscard]] bool operator==(const Vec3f& v) const noexcept;
+    [[nodiscard]] bool operator!=(const Vec3f& v) const noexcept;
+    [[nodiscard]] bool operator>(const Vec3f& v) const noexcept;
+    [[nodiscard]] bool operator<(const Vec3f& v) const noexcept;
+    [[nodiscard]] Vec3f operator-() const noexcept;
+    [[nodiscard]] float& operator[](uint32_t index) noexcept;
+    [[nodiscard]] float operator[](uint32_t index) const noexcept;
 
    public:
     friend std::ostream& operator<<(std::ostream& os, const Vec3f& vec);
     friend std::istream& operator>>(std::istream& is, Vec3f& vec);
 
    public:
-    static uint32_t dim();
-    static uint32_t size();
-    static Vec3f zero();
-    static Vec3f one();
-    static Vec3f xAxis();
-    static Vec3f yAxis();
-    static Vec3f zAxis();
-    static Vec3f up();
-    static Vec3f down();
-    static Vec3f left();
-    static Vec3f right();
-    static Vec3f forward();
-    static Vec3f backward();
-    static Vec3f normalized(const Vec3f& v);
-    static float distance(const Vec3f& v1, const Vec3f& v2);
-    static float dot(const Vec3f& v1, const Vec3f& v2);
-    static Vec3f cross(const Vec3f& v1, const Vec3f& v2);
-    static Vec3f midPoint(const Vec3f& p1, const Vec3f& p2);
-    static Vec3f lerp(const Vec3f& p1, const Vec3f& p2, float factor);
+    [[nodiscard]] static uint32_t dim() noexcept;
+    [[nodiscard]] static uint32_t size() noexcept;
+    [[nodiscard]] static Vec3f zero() noexcept;
+    [[nodiscard]] static Vec3f one() noexcept;
+    [[nodiscard]] static Vec3f xAxis() noexcept;
+    [[nodiscard]] static Vec3f yAxis() noexcept;
+    [[nodiscard]] static Vec3f zAxis() noexcept;
+    [[nodiscard]] static Vec3f up() noexcept;
+    [[nodiscard]] static Vec3f down() noexcept;
+    [[nodiscard]] static Vec3f left() noexcept;
+    [[nodiscard]] static Vec3f right() noexcept;
+    [[nodiscard]] static Vec3f forward() noexcept;
+    [[nodiscard]] static Vec3f backward() noexcept;
+    [[nodiscard]] static Vec3f normalized(const Vec3f& v);
+    [[nodiscard]] static float distance(const Vec3f& v1, const Vec3f& v2) noexcept;
+    [[nodiscard]] static float dot(const Vec3f& v1, const Vec3f& v2) noexcept;
+    [[nodiscard]] static Vec3f cross(const Vec3f& v1, const Vec3f& v2) noexcept;
+    [[nodiscard]] static Vec3f midPoint(const Vec3f& p1, const Vec3f& p2) noexcept;
+    [[nodiscard]] static Vec3f lerp(const Vec3f& p1, const Vec3f& p2, float factor) noexcept;
 };
 
 // Legacy type alias for backward compatibility
