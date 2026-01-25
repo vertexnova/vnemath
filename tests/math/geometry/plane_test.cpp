7571f205ca0ef7cf3b78b93dc9079eb9fca708ab
ayadav-macOs <yadav.ajeetsingh2020@gmail.com>
2026-01-25 06:27:45 -0500

    Update copyright year and creation date in geometry headers and source files
    Changes:
    - Updated copyright year from 2024 to 2026 in all geometry-related header and source files.
    - Adjusted creation date from January 2025 to January 2026 in the same files for consistency.

--- tests/math/geometry/plane_test.cpp
+++ tests/math/geometry/plane_test.cpp
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------
- * Copyright (c) 2024 Ajeet Singh Yadav. All rights reserved.
+ * Copyright (c) 2026 Ajeet Singh Yadav. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License")
  *
  * Author:    Ajeet Singh Yadav
- * Created:   January 2025
+ * Created:   January 2026
  *
  * Autodoc:   yes
  * ----------------------------------------------------------------------
